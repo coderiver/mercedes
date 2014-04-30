36013901e5113dbf3066f7187d2a766b72ddd7a3
Eugene Godun <eugene.godun@gmail.com>
2014-04-30 13:36:50 +0300

    index

--- js/common.js
+++ js/common.js
@@ -1,3 +1,6 @@
 head.ready(document, function () {
-	
+	$(".js-cars-key").on("click", function(){
+		$(this).parents(".range-cars").find(".js-cars-list").toggleClass("is-active");
+		return false;
+	});
 });
