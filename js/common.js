dd18209af585b3c3affe24b45378fcbf6b66aefe
Eugene Godun <eugene.godun@gmail.com>
2014-05-05 11:59:11 +0300

    models list

--- js/common.js
+++ js/common.js
@@ -7,4 +7,22 @@ head.ready(document, function () {
 		$(this).parent().find(".js-nav-car-list").slideToggle(200);
 		return false;
 	});
+
+
+	$(".js-gallery").each(function(){
+		var gallery = $(this);
+		var img = gallery.find(".js-gallery-img img");
+		var preview_first = gallery.find(".js-gallery-preview a").first();
+		var first_img = preview_first.attr("href");
+		preview_first.addClass("is-active");
+		img.attr("src", first_img);
+	});
+	$(".js-gallery-preview a").on("click", function(){
+		var img = $(this).attr("href");
+		$(".js-gallery-preview a").removeClass("is-active");
+		$(this).addClass("is-active");
+		$(this).parents(".js-gallery").find(".js-gallery-img img").attr("src", img)
+		return false;
+	});
+
 });
