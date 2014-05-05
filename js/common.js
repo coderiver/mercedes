019025bfcb7cef5a966500a305da4348f7f085b3
Eugene Godun <eugene.godun@gmail.com>
2014-05-05 10:31:48 +0300

    models

--- js/common.js
+++ js/common.js
@@ -3,4 +3,8 @@ head.ready(document, function () {
 		$(this).parents(".range-cars").find(".js-cars-list").toggleClass("is-active");
 		return false;
 	});
+	$(".js-nav-car").on("click", function(){
+		$(this).parent().find(".js-nav-car-list").slideToggle(200);
+		return false;
+	});
 });
