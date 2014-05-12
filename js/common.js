ffbee8e84c324c7418eaacb6425a09d106ea6e05
Splintex <eugene.godin@gmail.com>
2014-05-12 16:29:10 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,8 +1,4 @@
 head.ready(document, function () {
-	$(".js-cars-key").on("click", function(){
-		$(this).parents(".range-cars").find(".js-cars-list").toggleClass("is-active");
-		return false;
-	});
 	$(".js-nav-car").on("click", function(){
 		$(this).parent().find(".js-nav-car-list").slideToggle(200);
 		return false;
@@ -24,5 +20,5 @@ head.ready(document, function () {
 		$(this).parents(".js-gallery").find(".js-gallery-img img").attr("src", img)
 		return false;
 	});
-
+	
 });
